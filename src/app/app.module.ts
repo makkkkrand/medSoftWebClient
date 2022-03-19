05d813850954efae6d57431763c5025b1386c538
makkkkrand <makkkkrand@github.com>
2022-03-19 16:15:25 +0530

    Added new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { ErrorComponent } from './error/error.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,8 @@ import { LoginComponent } from './login/login.component';
     ErrorComponent,
     WelcomeComponent,
     LoginComponent,
+    FooterComponent,
+    HeaderComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
